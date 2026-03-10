a00681ba415e5fa8e9de5020f003ac8fe62d2af7
elanur321 <101106196+theoceancookie@users.noreply.github.com>
2026-03-10 11:51:36 +0100

    pong

---
+++ labb 4/build/obj/hw/tftlib/tft-md307.c.d
@@ -0,0 +1,2 @@
+build/obj/hw/tftlib/tft-md307.c.o: tftlib/tft-md307.c tftlib/md307.h \
+ tftlib/tftmd307.h
--- labb 4/src/main.c
+++ labb 4/src/main.c
@@ -61,12 +61,16 @@ typedef struct {
 typedef struct {
     int x;
     int y;
-    int speed_x;
+    int speed_y;
     int size;
+    int score;
 } Paddle;
 
-Paddle P1 = {width, 160, 0, 20};
-Paddle P2 = {0, 160, 0, 20};
+#define PWidth 10
+#define PSpeed 3
+Paddle P1 = {width-PWidth, 160, 0, 20};
+Paddle P2 = {PWidth, 160, 0, 20};
+
 
 typedef enum {
     UNBINDED,
@@ -76,15 +80,23 @@ typedef enum {
     P2DOWN,
 }keybindtype;
 
-Ball ball = {240, 160, 1, 0, 10};  // x, y, speed_x, speed_y, size
+Ball ball = {240, 160, 3, 0, 10};  // x, y, speed_x, speed_y, size
 
 uint32_t keybinds[16] = {
+    P2UP, 0, 0, P1UP,
     0, 0, 0, 0,
     0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0
+    P2DOWN, 0, 0, P1DOWN
 };
 
+
+void resetball(){
+    ball.x = 240;
+    ball.y = 160;
+    ball.speed_x = 3;
+    ball.speed_y = 1;
+    ball.size = 10;  // x, y, speed_x, speed_y, size
+}
 // volatile int counter = 0;
 
 __attribute__((interrupt("machine")))
@@ -95,23 +107,55 @@ void systick_handler() {
 
     //counter++;
 
-        // Erase old ball
-        tft_ellipse(ball.x, ball.y, ball.size, ball.size, COLOR_BLUE, 1);
+    // Erase old ball
+    tft_ellipse(ball.x, ball.y, ball.size, ball.size, COLOR_BLUE, 1);
 
-        // Bounce
-        if (ball.x <= ball.size || ball.x >= width - ball.size) {
+    //Paddle for player 1
+    tft_rect(P1.x, P1.y, PWidth, P1.size, COLOR_BLUE, 1 );
+
+    //Paddle for player 2
+    tft_rect(P2.x, P2.y, PWidth, P2.size, COLOR_BLUE, 1 );
+
+    
+
+    //Vänster collision
+    if (ball.x <= ball.size ) {
+        if(ball.y >= P2.y-(P2.size/2) && ball.y <= P2.y+(P2.size/2)){
             ball.speed_x = -ball.speed_x;
+        }else{//lose
+            P2.score++;
+            resetball();
         }
-        // if (ball.y <= ball.size || ball.y >= height - ball.size) {
-        //     ball.speed_y = -ball.speed_y;
-        // }
+        
+    }
+    if(ball.x >= width - ball.size){
+        if(ball.y >= P1.y-(P1.size/2) && ball.y <= P1.y+(P1.size/2)){
+            ball.speed_x = -ball.speed_x;
+        }else{//lose
+            P1.score++;
+            resetball();
+        }
+    }
+    if (ball.y <= ball.size || ball.y >= height - ball.size) {
+        ball.speed_y = -ball.speed_y;
+    }
 
-        // Move
-        ball.x += ball.speed_x;
-        // ball.y += ball.speed_y;
+    // Move
+    ball.x += ball.speed_x;
+    ball.y += ball.speed_y;
+    P1.y += P1.speed_y;
+    P2.y += P2.speed_y;
 
-        // Draw new ball
-        tft_ellipse(ball.x, ball.y, ball.size, ball.size, COLOR_WHITE, 1);
+    //DRAW
+
+    //Paddle for player 1
+    tft_rect(P1.x, P1.y, PWidth, P1.size, COLOR_WHITE, 1 );
+
+    //Paddle for player 2
+    tft_rect(P2.x, P2.y, PWidth, P2.size, COLOR_WHITE, 1 );
+
+    //ball
+    tft_ellipse(ball.x, ball.y, ball.size, ball.size, COLOR_WHITE, 1);
     STK->SR &= ~(1 << 0);
 
 }
@@ -119,103 +163,43 @@ __attribute__((interrupt("machine")))
 void exti_handler() {
     uint8_t key = keypad();
 
-  if (key != 0xFF) {
-    // Key pressed - start tone and show key number (1-16) on bargraph
-    systick_periodic_micro(periods[key]);
-    GPIOD->ODR = (GPIOD->ODR & 0x00FF) | ((key + 1) << 8); // & 0x00FF preserves pins D0–D7 (keypad pins), then (key + 1) << 8 writes the key number in binary to pins D8–D15 (bargraph)
-  } else {
+    if (key != 0xFF) {
+        // Key pressed - start tone and show key number (1-16) on bargraph
+        if(keybinds[key]=P1DOWN){
+            P1.speed_y = PSpeed;
+        }else if(keybinds[key]=P1UP){
+            P1.speed_y = -PSpeed;
+        }else if(keybinds[key]=P2DOWN){
+            P2.speed_y = PSpeed;
+        }else if(keybinds[key]=P2UP){
+            P2.speed_y = -PSpeed;
+        }
+        GPIOD->ODR = (GPIOD->ODR & 0x00FF) | ((key + 1) << 8); // & 0x00FF preserves pins D0–D7 (keypad pins), then (key + 1) << 8 writes the key number in binary to pins D8–D15 (bargraph)
+    } else {
     // Key released - stop tone and turn off bargraph
-    systick_stop();
+  
     GPIOD->ODR = (GPIOD->ODR & 0x00FF);                      
-  }
+    }
+}
 
-int main(void)
-{
-     init_gpio();
-     init_vector_table();
-     init_interrupts();
-     tft_init();
-    
-    ///////////////////////////////////////////////////////////////////////////
-    // Assignment 1: Initialize the display and fill it with a solid color
-    // =======================================================================
-    // Once tft_init() has completed, the screen should be all yellow. Fill it 
-    // with a black background color (use COLOR_BLACK as the color argument) 
-    // using tft_rect().
-     //tft_rect(x1, y1, w, h, color, filled) to draw a rectangle (filled or not)
-    // Note: On the simulator, w has to be given as 479, and h as 319, to fill
-    //       the whole screen. 
-    ///////////////////////////////////////////////////////////////////////////
-
-    //extern void (*tft_rect)(int x, int y, int w, int h, uint16_t color, int filled);
+int main(void){
+    init_gpio();
+    init_vector_table();
+    init_interrupts();
+    tft_init();
 
 
     tft_rect(0,0,479,319,COLOR_BLUE, 1);
 
-
-    
-    ///////////////////////////////////////////////////////////////////////////
-    // Assignment 2: Draw a ball in the middle of the screen
-    // =======================================================================
-    // Use the tft_ellipse() function to draw a ball in the middle of 
-    // the screen. Choose any color for the ball. It should have  
-    // The colors are in RGB565 format, which is a common 16-bit color format 
-    // used in embedded graphics. It means that the most significant 5 bits are red,    
-    // the next 6 bits are green, and the least significant 5 bits are blue.
-    // 
-    // You can use the RGB565(r, g, b) macro above to convert 8-bit RGB values 
-    // to RGB565 format. For example, RGB565(255, 0, 0) will give you the 
-    // RGB565 value for pure red.
-    ///////////////////////////////////////////////////////////////////////////
-
-    //tft_ellipse(ball.x, ball.y, ball.size, ball.size, RGB565(255, 255, 255), 1);
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Assignment 3: Make it move!
-    // =======================================================================
-    // Now, make the ball move across the screen from left to right. We have 
-    // given you a simple loop that will calculate the center and bounce on 
-    // the edges, so just fill in the drawing code. 
-    // 
-    // A few hints: 
-    // - Drawing the ball does not erase the previous one. On desktop games
-    //   we usually clear the whole screen every frame. You can try that, but
-    //   you will find that it is very slow. Instead, just erase the previous
-    //   ball by drawing it again, in the background color. 
-    // - When you do that, there will be flickering. To reduce the flickering
-    //   you can pause for a short time after drawing the ball, before erasing 
-    //   it. Use SysTick, or just a simple for-loop delay. 
-    // - When you run this on hardware, timings might be very different. 
-    ///////////////////////////////////////////////////////////////////////////
 
     systick_periodic_micro(32000);  // 16000 ~60fps
 
     
 
     while(1) {
-    // if (update == 1) {
-    //     update = 0;
-    //     // counter++;
+  
+    }
 
-    //     // // Erase old ball
-    //     // tft_ellipse(ball.x, ball.y, ball.size, ball.size, COLOR_BLUE, 1);
-
-    //     // // Bounce
-    //     // if (ball.x <= ball.size || ball.x >= width - ball.size) {
-    //     //     ball.speed_x = -ball.speed_x;
-    //     // }
-    //     // // if (ball.y <= ball.size || ball.y >= height - ball.size) {
-    //     // //     ball.speed_y = -ball.speed_y;
-    //     // // }
-
-    //     // // Move
-    //     // ball.x += ball.speed_x;
-    //     // // ball.y += ball.speed_y;
-
-    //     // // Draw new ball
-    //     // tft_ellipse(ball.x, ball.y, ball.size, ball.size, COLOR_WHITE, 1);
-    // }
-}
 
     ///////////////////////////////////////////////////////////////////////////
     // Assignment 3: Make it bounce in 2D!
@@ -250,4 +234,5 @@ int main(void)
     // 
     // See the canvas page for instructions and points. 
     ///////////////////////////////////////////////////////////////////////////
+    return 0;
 }
